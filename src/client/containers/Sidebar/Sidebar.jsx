9fab837fa0fc6136da4599fec2917b179350ee9c
Yukioru <obt195@gmail.com>
2017-05-21 15:47:15 +0400

    Перенес другие папки в common

---
+++ src/client/containers/Sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class Sidebar extends PureComponent {
+    render() {
+        return (
+            <nav className="nav-sidebar">
+                123
+            </nav>
+        );
+    }
+}
+
+export default Sidebar;
