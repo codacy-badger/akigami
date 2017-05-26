2ebd6553033ccc4ab0fa48552d707d267d3051f9
Yukioru <obt195@gmail.com>
2017-05-26 13:31:52 +0400

    Шаги авторизации в модалке

--- src/client/containers/AuthModal/AuthModal.jsx
+++ src/client/containers/AuthModal/AuthModal.jsx
@@ -11,28 +11,55 @@ import {
     Input,
 } from 'reactstrap';
 
+const exampleCode = 'c427af39-8259-4d95-a79e-77610b797194';
+
 class AuthModal extends PureComponent {
     static propTypes = {
         onHide: PropTypes.func.isRequired,
         modal: PropTypes.bool.isRequired,
     }
     state = {
+        step: 'notLogged',
         email: '',
+        authCode: '',
+        backdrop: true,
     };
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextState.authCode === exampleCode) {
+            return this.handleConfirm();
+        }
+        return true;
+    }
+    onChange = action => (e) => {
+        this.setState({ [action]: e.target.value });
+    }
+    handleConfirm = () => {
+        alert('confirmed!');
+        this.reset();
+    }
     handleAuth = () => {
         alert('auth!');
+        this.setState({
+            step: 'confirm',
+            backdrop: 'static',
+        });
+    }
+    reset = () => {
         this.props.onHide();
+        this.setState({
+            backdrop: true,
+            step: 'notLogged',
+            email: '',
+            authCode: '',
+        });
     }
-    changeEmail = (e) => {
-        this.setState({ email: e.target.value });
-    }
-    render() {
+    renderNotLogged() {
         const { email } = this.state;
-        const { modal, onHide } = this.props;
+        const { onHide } = this.state;
         return (
-            <Modal modalClassName="centered" isOpen={modal} toggle={onHide}>
+            <div>
                 <ModalHeader toggle={this.toggle} style={{ justifyContent: 'center' }}>
-                    Авторизация
+                    Вход / Регистрация
                 </ModalHeader>
                 <ModalBody>
                     <p>Введите свой электронный ящик, чтобы войти или зарегистрироваться</p>
@@ -43,7 +70,7 @@ class AuthModal extends PureComponent {
                             name="email"
                             id="authEmail"
                             value={email}
-                            onChange={this.changeEmail}
+                            onChange={this.onChange('email')}
                             placeholder="Например: suzuki@chan.jp"
                         />
                     </FormGroup>
@@ -58,6 +85,54 @@ class AuthModal extends PureComponent {
                         Войти
                     </Button>
                 </ModalFooter>
+            </div>
+        );
+    }
+    renderConfirm() {
+        const { authCode } = this.state;
+        return (
+            <div>
+                <ModalHeader style={{ justifyContent: 'center' }}>
+                    Подтверждение входа
+                </ModalHeader>
+                <ModalBody>
+                    <p style={{ marginBottom: 0 }}>
+                        Проверьте свой электронный ящик. Вам пришло письмо с подтверждением авторизации.
+                    </p>
+                    <small style={{ display: 'inline-block', marginBottom: 0 }}>
+                        Перейдите по ссылке в письме или введите код авторизации ниже.
+                    </small>
+                    <FormGroup>
+                        <Label for="authEmail">Код авторизации</Label>
+                        <Input
+                            type="email"
+                            name="email"
+                            id="authEmail"
+                            value={authCode}
+                            onChange={this.onChange('authCode')}
+                            placeholder="Например: c427af39-8259-4d95-a79e-77610b797194"
+                        />
+                    </FormGroup>
+                </ModalBody>
+                <ModalFooter style={{ justifyContent: 'center' }}>
+                    <Button color="link" onClick={this.reset}>Отменить авторизацию</Button>
+                </ModalFooter>
+            </div>
+        );
+    }
+    render() {
+        const { step, backdrop } = this.state;
+        const { modal, onHide } = this.props;
+        const isToggle = backdrop !== 'static';
+        return (
+            <Modal
+                modalClassName="centered"
+                isOpen={modal}
+                toggle={isToggle && onHide}
+                backdrop={backdrop}
+            >
+                {step === 'notLogged' && this.renderNotLogged()}
+                {step === 'confirm' && this.renderConfirm()}
             </Modal>
         );
     }
