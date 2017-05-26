8ec86604bf09995c53fea60c76d2964bad2737cd
Yukioru <obt195@gmail.com>
2017-05-26 14:48:07 +0400

    Логика и шаги регистрации в авторизационной модалке

--- src/client/containers/AuthModal/AuthModal.jsx
+++ src/client/containers/AuthModal/AuthModal.jsx
@@ -9,40 +9,94 @@ import {
     FormGroup,
     Label,
     Input,
+    FormFeedback,
+    FormText,
 } from 'reactstrap';
 
-const exampleCode = 'c427af39-8259-4d95-a79e-77610b797194';
+const exampleRegisterCode = 'a9b5d377-3849-459c-9767-89237d659de6';
+const exampleLoginCode = 'c427af39-8259-4d95-a79e-77610b797194';
+
+const isCode = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
+const isEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 class AuthModal extends PureComponent {
+    static defaultProps = {
+        step: null,
+        backdrop: null,
+    }
     static propTypes = {
         onHide: PropTypes.func.isRequired,
         modal: PropTypes.bool.isRequired,
+        step: PropTypes.oneOf(['notLogged', 'confirm', 'register']),
+        backdrop: PropTypes.oneOfType([PropTypes.bool, PropTypes.oneOf(['static'])]),
     }
-    state = {
-        step: 'notLogged',
-        email: '',
-        authCode: '',
-        backdrop: true,
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            step: props.step || 'notLogged',
+            email: '',
+            authCode: '',
+            username: '',
+            birthday: '',
+            gender: 'none',
+            usernameStatus: null,
+            emailStatus: null,
+            codeStatus: null,
+            backdrop: props.backdrop || true,
+        };
+    }
     shouldComponentUpdate(nextProps, nextState) {
-        if (nextState.authCode === exampleCode) {
-            return this.handleConfirm();
+        const state = nextState;
+        if (isCode.test(nextState.authCode)) {
+            state.codeStatus = null;
+            if (nextState.authCode === exampleLoginCode) {
+                this.handleConfirm();
+            }
+            if (nextState.authCode === exampleRegisterCode) {
+                state.step = 'register';
+            }
+        } else if (nextState.authCode.length === 0) {
+            state.codeStatus = null;
+        } else {
+            state.codeStatus = 'danger';
         }
-        return true;
+
+        if (isEmail.test(nextState.email)) {
+            state.emailStatus = 'success';
+        } else {
+            state.emailStatus = null;
+        }
+
+        if (nextState.username.length > 3) {
+            state.usernameStatus = 'success';
+        } else {
+            state.usernameStatus = null;
+        }
+
+        return state;
     }
     onChange = action => (e) => {
         this.setState({ [action]: e.target.value });
     }
+    handleRegister = () => {
+        const { usernameStatus } = this.state;
+        if (usernameStatus) {
+            this.reset();
+        }
+    }
     handleConfirm = () => {
-        alert('confirmed!');
+        alert('confirmed login!');
         this.reset();
     }
     handleAuth = () => {
         alert('auth!');
-        this.setState({
-            step: 'confirm',
-            backdrop: 'static',
-        });
+        const { emailStatus } = this.state;
+        if (emailStatus) {
+            this.setState({
+                step: 'confirm',
+                backdrop: 'static',
+            });
+        }
     }
     reset = () => {
         this.props.onHide();
@@ -51,10 +105,15 @@ class AuthModal extends PureComponent {
             step: 'notLogged',
             email: '',
             authCode: '',
+            username: '',
+            usernameStatus: null,
+            emailStatus: null,
+            gender: 'none',
+            birthday: '',
         });
     }
     renderNotLogged() {
-        const { email } = this.state;
+        const { email, emailStatus } = this.state;
         const { onHide } = this.state;
         return (
             <div>
@@ -63,13 +122,14 @@ class AuthModal extends PureComponent {
                 </ModalHeader>
                 <ModalBody>
                     <p>Введите свой электронный ящик, чтобы войти или зарегистрироваться</p>
-                    <FormGroup>
+                    <FormGroup color={emailStatus}>
                         <Label for="authEmail">Email</Label>
                         <Input
                             type="email"
                             name="email"
                             id="authEmail"
                             value={email}
+                            state={emailStatus}
                             onChange={this.onChange('email')}
                             placeholder="Например: suzuki@chan.jp"
                         />
@@ -81,6 +141,7 @@ class AuthModal extends PureComponent {
                         color="danger"
                         onClick={this.handleAuth}
                         style={{ width: '40%' }}
+                        disabled={!emailStatus}
                     >
                         Войти
                     </Button>
@@ -89,7 +150,7 @@ class AuthModal extends PureComponent {
         );
     }
     renderConfirm() {
-        const { authCode } = this.state;
+        const { authCode, codeStatus } = this.state;
         return (
             <div>
                 <ModalHeader style={{ justifyContent: 'center' }}>
@@ -99,23 +160,91 @@ class AuthModal extends PureComponent {
                     <p style={{ marginBottom: 0 }}>
                         Проверьте свой электронный ящик. Вам пришло письмо с подтверждением авторизации.
                     </p>
-                    <small style={{ display: 'inline-block', marginBottom: 0 }}>
+                    <small style={{ display: 'inline-block', marginBottom: '1rem' }}>
                         Перейдите по ссылке в письме или введите код авторизации ниже.
                     </small>
-                    <FormGroup>
-                        <Label for="authEmail">Код авторизации</Label>
+                    <FormGroup color={codeStatus}>
+                        <Label for="authCode">Код авторизации</Label>
                         <Input
-                            type="email"
-                            name="email"
-                            id="authEmail"
+                            type="text"
+                            name="text"
+                            id="authCode"
                             value={authCode}
+                            state={codeStatus}
                             onChange={this.onChange('authCode')}
                             placeholder="Например: c427af39-8259-4d95-a79e-77610b797194"
                         />
+                        {codeStatus === 'danger' && (
+                            <FormFeedback>Вы ввеели не код подтверждения</FormFeedback>
+                        )}
                     </FormGroup>
                 </ModalBody>
                 <ModalFooter style={{ justifyContent: 'center' }}>
                     <Button color="link" onClick={this.reset}>Отменить авторизацию</Button>
+                </ModalFooter>
+            </div>
+        );
+    }
+    renderRegister() {
+        const { birthday, gender, username, usernameStatus } = this.state;
+        return (
+            <div>
+                <ModalHeader style={{ justifyContent: 'center' }}>
+                    Регистрация
+                </ModalHeader>
+                <ModalBody>
+                    <p>Заполните информацию для аккаунта.</p>
+                    <FormGroup color={usernameStatus}>
+                        <Label for="username">Имя пользователя</Label>
+                        <Input
+                            type="text"
+                            name="text"
+                            id="username"
+                            value={username}
+                            state={usernameStatus}
+                            onChange={this.onChange('username')}
+                            placeholder="Например: Joker"
+                        />
+                        {usernameStatus === 'danger' && (
+                            <FormFeedback>Это поле не может быть пустым</FormFeedback>
+                        )}
+                        <FormText color="muted">* поле обязательно для заполнения</FormText>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="gender">Пол</Label>
+                        <Input
+                            type="select"
+                            name="select"
+                            id="gender"
+                            value={gender}
+                            onChange={this.onChange('gender')}
+                        >
+                            <option value="none">Не определился</option>
+                            <option value="male">Мужской</option>
+                            <option value="female">Женский</option>
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="birthday">День рождения</Label>
+                        <Input
+                            type="date"
+                            name="date"
+                            id="birthday"
+                            value={birthday}
+                            onChange={this.onChange('birthday')}
+                            placeholder="Например: 01.01.1995"
+                        />
+                    </FormGroup>
+                </ModalBody>
+                <ModalFooter style={{ justifyContent: 'center' }}>
+                    <Button color="link" onClick={this.reset}>Отменить</Button>
+                    <Button
+                        color="success"
+                        onClick={this.handleRegister}
+                        disabled={!usernameStatus}
+                    >
+                        Зарегистрироваться
+                    </Button>
                 </ModalFooter>
             </div>
         );
@@ -133,6 +262,7 @@ class AuthModal extends PureComponent {
             >
                 {step === 'notLogged' && this.renderNotLogged()}
                 {step === 'confirm' && this.renderConfirm()}
+                {step === 'register' && this.renderRegister()}
             </Modal>
         );
     }
