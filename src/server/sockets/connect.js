7267602fadbaf4345505963a43d97f0c654109cd
Yukioru <obt195@gmail.com>
2017-07-23 13:01:32 +0400

    Реквайр сокетов

--- src/server/sockets/connect.js
+++ src/server/sockets/connect.js
@@ -1,10 +1,10 @@
-import auth from './routes/auth';
+import { requireFiles } from '../utils';
 
 export default (socket) => {
-    auth(socket);
+    requireFiles('sockets/routes', socket);
     console.log(`Socket ${socket.conn.id} connected`);
 
     socket.on('disconnect', () => {
         console.log(`Socket ${socket.conn.id} disconnected`);
     });
-}
+};
