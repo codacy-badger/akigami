33beec1eb1d5ad79facacef246d3bde196ff560f
Andrey Kondalov <i@yukio.ru>
2018-08-02 19:08:45 +0400

    restore license and more

---
+++ README.md
@@ -0,0 +1,6 @@
+
+[![Travis-CI Status](https://travis-ci.org/akigami/akigami.svg?branch=master)](https://travis-ci.org/akigami/akigami)
+
+
+### Документация
+https://github.com/akigami/akigami/wiki/Главная
---
+++ tests/statuses.test.js
@@ -0,0 +1,11 @@
+const statuses = [
+    'watching',
+    'completed',
+    'planned',
+    'dropped',
+    'paused',
+  ];
+  
+  test('in statuses has completed on it', () => {
+    expect(statuses).toContain('completed');
+  });
