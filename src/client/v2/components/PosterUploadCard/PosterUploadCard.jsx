cf901e29589fe7678ed4095b54c167fa55b2c9c2
Andrey Kondalov <i@yukio.ru>
2018-11-17 22:03:12 +0400

    PosterUploadCard компонент для выбора постеров

---
+++ src/client/v2/components/PosterUploadCard/PosterUploadCard.jsx
@@ -0,0 +1,92 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { Dimmer, Loader, Header, Icon, Divider, Segment, Button } from 'semantic-ui-react';
+import Dropzone from 'react-dropzone';
+import cx from 'classnames';
+
+import Inline from '../Inline';
+
+class PosterUploader extends PureComponent {
+  static propTypes = {
+    src: PropTypes.string,
+    onChange: PropTypes.func,
+  }
+  static defaultProps = {
+    src: null,
+    onChange: null,
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+    };
+    this.handleDropImage = this.handleDropImage.bind(this);
+  }
+  async handleDropImage(files) {
+    const { onChange } = this.props;
+    this.setState({ loading: true });
+    if (onChange) {
+      await onChange(files[0]);
+      this.setState({ loading: false });
+    }
+  }
+  render() {
+    const { loading } = this.state;
+    const { src } = this.props;
+    return (
+      <Segment className="segment-no-padding poster-upload" style={{ margin: 0 }}>
+        <Dropzone
+          className="dropzone"
+          onDrop={this.handleDropImage}
+          disableClick
+        >
+          {({ open, isDragActive }) => {
+            if (isDragActive) {
+              return (
+                <Inline
+                  align="center"
+                  style={{
+                    height: '100%',
+                    justifyContent: 'center',
+                    backgroundColor: 'rgba(0, 0, 0, 0.05)',
+                  }}
+                >
+                  <Header as="h4" icon>
+                    <Icon name="download" />
+                    Переместите изображение сюда
+                  </Header>
+                </Inline>
+              );
+            }
+            return (
+              <Inline align="center">
+                <div className="poster-upload-image">
+                  <Dimmer active={loading} inverted>
+                    <Loader inverted content="Загрузка" />
+                  </Dimmer>
+                  <div
+                    className="poster"
+                    style={{
+                      backgroundImage: `url(${(src && src.preview) || src})`,
+                    }}
+                  />
+                </div>
+                <div className="poster-upload-content">
+                  <Dimmer active={loading} inverted />
+                  <Button type="button" animated="fade" onClick={() => open()}>
+                    <Button.Content visible>Загрузите изображение</Button.Content>
+                    <Button.Content hidden>Выбрать файл</Button.Content>
+                  </Button>
+                  <Divider horizontal>Или</Divider>
+                  <p>переместите его сюда</p>
+                </div>
+              </Inline>
+            );
+          }}
+        </Dropzone>
+      </Segment>
+    );
+  }
+}
+
+export default PosterUploader;
