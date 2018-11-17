be3e398d9eb29f28257be211440fb70347f63aef
Andrey Kondalov <i@yukio.ru>
2018-11-17 15:27:02 +0400

    Inline компонент

---
+++ src/client/v2/components/Inline/Inline.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+class Inline extends Component {
+  static propTypes = {
+    align: PropTypes.oneOf(['top', 'center', 'bottom']),
+    children: PropTypes.any.isRequired,
+  }
+  static defaultProps = {
+    align: 'top',
+  }
+  render() {
+    const { children, align } = this.props;
+    return (
+      <div
+        className={cx({
+          'inline-block': true,
+          [`align-${align}`]: align,
+        })}
+      >
+        {children}
+      </div>
+    );
+  }
+}
+
+export default Inline;
