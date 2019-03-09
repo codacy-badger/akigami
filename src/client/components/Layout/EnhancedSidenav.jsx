d856494a39d2b1cfc570c3deb8646e8c825efcea
Andrey Kondalov <i@yukio.ru>
2019-03-10 01:09:15 +0400

    Remove useless code

--- src/client/components/Layout/EnhancedSidenav.jsx
+++ src/client/components/Layout/EnhancedSidenav.jsx
@@ -63,9 +63,6 @@ class EnhancedSidenav extends Component {
         }
       }
       this.canUseOutsideEvent = this.memScreenWidth <= this.switchPoint;
-      if (!this.canUseOutsideEvent && ui.isOpenSidenav) {
-        ui.closeSidenav();
-      }
     }
   }
 
